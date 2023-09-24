4c9fa2aa7a09118e02537f65f6b28737545686f9
shivansh13579 <shivansh0975@gmail.com>
2023-09-24 07:16:20 +0530

    added addTodo and removeTodo function

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodos from './components/AddTodos'
+import Todos from './components/todos'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,8 @@ function App() {
   return (
     <>
       <h1>shivam learns a redux toolkit</h1>
+      <AddTodos/>
+      <Todos/>
     </>
   )
 }
